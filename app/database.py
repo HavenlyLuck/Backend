ce22c918e80afbaed8cafe3041d0786fafd293f0
Zoonhyeong <wkcpq35@naver.com>
2026-08-14 10:30:21 +0900

    feat. DB 커넥션 풀 문제 해결

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 
 DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=280)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
