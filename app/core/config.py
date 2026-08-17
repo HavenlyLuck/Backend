4584604aa265a507788c8f360f7e73499865cd01
Zoonhyeong <wkcpq35@naver.com>
2026-08-17 16:40:47 +0900

    feat. refresh token 로직 구현

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 14  # 14일
     CLOUDINARY_CLOUD_NAME: str
     CLOUDINARY_API_KEY: str
     CLOUDINARY_API_SECRET: str
